4a6605c7b7b292d11425c5d4b8dba1c2a9bdeeb8
Lucas <111348190+FatBabyYoda@users.noreply.github.com>
2025-01-06 17:18:26 +0100

    Project fixat med saker som saknas
    Även kod för att lägga till ej testad men inga error för mig

--- CV-Website/Controllers/ProjectController.cs
+++ CV-Website/Controllers/ProjectController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CV_Website.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NuGet.Packaging.Signing;
 
 namespace CV_Website.Controllers
 {
     public class ProjectController : Controller
     {
+        private CVContext _context;
+
+        public ProjectController(CVContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            Project project = new Project();       
+            return View(project);
+        }
+        [HttpPost]
+        public IActionResult Add(Project project)
+        {
+
+            _context.Add(project);
+            _context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            
+            
+
         }
     }
 }
--- CV-Website/Models/Project.cs
+++ CV-Website/Models/Project.cs
@@ -5,7 +5,7 @@
         public int ProjectId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-
+        public string Information { get; set; }
         public int CreatorId { get; set; }
         public virtual ICollection<User> User { get; set; } = new List<User>();
     }
