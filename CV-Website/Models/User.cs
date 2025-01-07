bbfcbb5dffe1b8fd446b8b507f470079c7f82ea8
DubberOG <154241815+DubberOG@users.noreply.github.com>
2025-01-07 10:48:57 +0100

    ss

--- CV-Website/Models/User.cs
+++ CV-Website/Models/User.cs
@@ -1,11 +1,20 @@
-﻿namespace CV_Website.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CV_Website.Models
 {
     public class User
     {
         public int UserId { get; set; }
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Du kan inte logga in utan ett lösenord")]
         public string Password { get; set; }
+
+        [StringLength(50, ErrorMessage = "Ange en giltig adress")]
         public string Address { get; set; }
+
+        [Required(ErrorMessage = "Du måste ange en e-postadress")]
         public string Email { get; set; }
         public Boolean Private { get; set; }
 
