e09ac8f1c637c23060b8600aba36c9fb84c52380
Lucas <111348190+FatBabyYoda@users.noreply.github.com>
2025-01-07 18:08:39 +0100

    Project views och metoder

--- CV-Website/Controllers/ProjectController.cs
+++ CV-Website/Controllers/ProjectController.cs
@@ -1,8 +1,13 @@
-﻿using CV_Website.Models;
+﻿using System.Security.Claims;
+using CV_Website.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using NuGet.Packaging.Signing;
+using Project = CV_Website.Models.Project;
 
 namespace CV_Website.Controllers
 {
@@ -20,6 +25,7 @@ namespace CV_Website.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IActionResult CreateProject()
         {
             Project project = new Project();
@@ -28,10 +34,18 @@ namespace CV_Website.Controllers
 
           
         }
+        //lägg till inlogad användare istllet för hårdkodningen
         [HttpPost]
-        public IActionResult Add(Project project)
+        [Authorize]
+        public IActionResult CreateProject(Project project)
         {
-            project.CreatorId = 2;
+            // ej testad kod
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            int loggedInUserId = int.Parse(userId);
+            //project.CreatorId = 2;
+
+            project.CreatorId = loggedInUserId; // denna rad 
+           
             _context.Add(project);
             _context.SaveChanges();
                 return RedirectToAction("Index", "Home");
@@ -39,5 +53,85 @@ namespace CV_Website.Controllers
             
 
         }
+
+
+        //auth kolla användare som skapat är den inloggade???
+        [HttpGet]
+        [Authorize]
+        [Route("Project/EditProject/{projectId}")]
+        public IActionResult EditProject(int projectId)
+        {
+            
+            var project = _context.Project.FirstOrDefault(u => u.ProjectId == projectId);
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            int loggedInUserId = int.Parse(userId);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            if (project.CreatorId != loggedInUserId)
+            {
+                return Unauthorized();
+            }
+
+            return View("EditProject", project); 
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult EditProject(int projectId, Project updatedProject)
+        {
+            if (projectId != updatedProject.ProjectId)
+            {
+                return BadRequest();
+            }
+
+            var project = _context.Project.FirstOrDefault(u => u.ProjectId == projectId);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            project.Title = updatedProject.Title;
+            project.Description = updatedProject.Description;
+            project.Information = updatedProject.Information;
+            
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public IActionResult ListProject()
+        {
+            IQueryable<Project> ProjectList = from project in _context.Project select project;
+            
+            return View(ProjectList.ToList());
+        }
+        // om personen inte är inloggad ska privata profiler tas bort!!
+        public IActionResult ProjectPage(int Id)
+        {
+            var project = _context.Project
+        .Include(p => p.Users) 
+        .Include(p => p.Creator) 
+        .FirstOrDefault(u => u.ProjectId == Id);
+
+
+            if (!User.Identity.IsAuthenticated)// ej testad
+            {
+                project.Users = project.Users.Where(u => !u.Private).ToList();
+            }
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+
+            return View(project);
+        }
+
+        
     }
 }
