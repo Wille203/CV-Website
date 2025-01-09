c636faec8d90525349e649520a5ae8d1327a59b1
DubberOG <154241815+DubberOG@users.noreply.github.com>
2025-01-09 19:11:49 +0100

    cv

---
+++ CV-Website/Controllers/CVController.cs
@@ -0,0 +1,17 @@
+﻿using CV_Website.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV_Website.Controllers
+{
+    public class CVController : Controller
+    {
+        private CVContext _context;
+
+        public CVController(CVContext context) 
+        {
+            _context = context;
+        }
+
+       
+    }
+}
---
+++ CV-Website/ViewModels/CVViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CV_Website.Models;
+
+namespace CV_Website.ViewModels
+{
+    public class CVViewModel
+    {
+        public int UserId { get; set; }
+        public List<Skills> AllSkills { get; set; } = new List<Skills>();
+        public List<Education> AllEducations { get; set; } = new List<Education>();
+        public List<Experience> AllExperiences { get; set; } = new List<Experience>();
+
+        // Användarens nuvarande val 
+        public List<int> SelectedSkills { get; set; } = new List<int>();  
+        public List<int> SelectedEducations { get; set; } = new List<int>();  
+        public List<int> SelectedExperiences { get; set; } = new List<int>();  
+    }
+}
