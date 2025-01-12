0af2216287ec81624ac6a8eabeb757b5b99696b3
Wille203 <153735922+Wille203@users.noreply.github.com>
2025-01-13 00:23:29 +0100

    Confirmation delete
    Added confirmation when deleting messages

--- CV-Website/wwwroot/js/site.js
+++ CV-Website/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//Visar Bekräfta och avbryt knapparna när man vill ta bort meddelande
+function showConfirmation(button) {
+    button.style.display = 'none'; 
+    button.nextElementSibling.style.display = 'block'; 
+}
+
+function hideConfirmation(button) {
+    var confirmationButtons = button.parentElement; // Föräldern är 'confirmation-buttons'
+    var deleteButton = confirmationButtons.previousElementSibling; // Föregående syskon är 'delete-button'
+    confirmationButtons.style.display = 'none'; // Dölj bekräftelseknapparna
+    deleteButton.style.display = 'block'; // Visa 'Ta bort'-knappen
+}
