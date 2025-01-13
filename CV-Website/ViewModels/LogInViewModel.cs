8d5552c39d8eabdacd1bb78c41da26db55ee84db
Lucas <111348190+FatBabyYoda@users.noreply.github.com>
2025-01-13 15:21:02 +0100

    Svenska label fix

--- CV-Website/ViewModels/LogInViewModel.cs
+++ CV-Website/ViewModels/LogInViewModel.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace CV_Website.ViewModels
@@ -7,10 +8,13 @@ namespace CV_Website.ViewModels
     {
         [Required(ErrorMessage = "Vänligen skriv en UserName.")]
         [StringLength(255)]
+        [DisplayName("Email")]
         public string UserName { get; set; }
         [Required(ErrorMessage = "Vänligen skriv ett lösenord.")]
         [DataType(DataType.Password)]
+        [DisplayName("Lösenord")]
         public string Password { get; set; }
+        [DisplayName("Kom ihåg mig")]
         public bool RememberMe { get; set; }
     }
 }
